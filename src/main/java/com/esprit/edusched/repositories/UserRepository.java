bb3656763fa584c11c9582482275058de76db1ac
sebtiranime <125137962+sebtiranime@users.noreply.github.com>
2024-05-06 23:02:13 +0100

    Update UserRepository.java

--- src/main/java/com/esprit/edusched/repositories/UserRepository.java
+++ src/main/java/com/esprit/edusched/repositories/UserRepository.java
@@ -1,17 +1,81 @@
-package com.esprit.edusched.repositories;
+package com.esprit.edusched.services;
 
-import com.esprit.edusched.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.esprit.edusched.entities.Bloc;
+import com.esprit.edusched.repositories.BlocRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-    boolean existsByEmail(String email);
-
-    Optional<User> findByEmail(String email);
-    //User findByEmail1(String email);
+@Service
+@AllArgsConstructor
 
 
-}
+public class BlocService implements IBlocService{
+
+    private BlocRepository blocRepository;
+
+    @Override
+    public List<Bloc> getAllBlocs() {
+        return blocRepository.findAll();
+    }
+
+    @Override
+    public Bloc getBlocById(Long id) {
+        return blocRepository.findById(id).orElse(null);    }
+
+    @Override
+    public Bloc addBloc(Bloc bloc) {
+        return blocRepository.save(bloc);
+    }
+
+    @Override
+    public String deleteBloc(Long id) {
+        if (blocRepository.existsById(id)) {
+            blocRepository.deleteById(id);
+            return "Bloc deleted";
+        }
+        return "Bloc not found";
+    }
+
+
+    @Override
+    public Bloc updateBloc(Long id, Bloc updatedBloc) {
+        Bloc bloc = blocRepository.findById(id).orElse(null);
+        if (bloc != null) {
+            bloc.setName(updatedBloc.getName());
+            bloc.setClasses(updatedBloc.getClasses());
+            return blocRepository.save(bloc);
+        }
+        return null;    }
+
+/*
+    @Override
+    public Bloc getBlocById(Long id) {
+        return blocRepository.findById(id).orElse(null);
+    }*/
+
+   /* @Override
+    public Bloc updateBloc(Long id, Bloc updatedBloc) {
+        Bloc bloc = blocRepository.findById(id).orElse(null);
+        if (bloc != null) {
+            bloc.setName(updatedBloc.getName());
+            bloc.setClasses(updatedBloc.getClasses());
+            return blocRepository.save(bloc);
+        }
+        return null;
+<<<<<<< HEAD
+    }*/
+
+    /*@Override
+=======
+    }
+    @Override
+>>>>>>> wejdene-creates-crud-v1.3
+    public String deleteBloc(Long id) {
+        if (blocRepository.existsById(id)) {
+            blocRepository.deleteById(id);
+            return "Bloc deleted";
+        }
+        return "Bloc not found";
+    }*/
